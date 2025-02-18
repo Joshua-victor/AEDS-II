c15a21862dbd74ea33056f4a58191fa3c454a58f
Joshua Victor <80266019+Joshua-victor@users.noreply.github.com>
2025-02-18 08:18:36 -0300

    Add files via upload

---
+++ TPs/TP1/TP01Q03.java
@@ -0,0 +1,32 @@
+public class TP01Q03
+{
+	public static void main(String[] args) {
+
+		boolean resp = true; // bool para a condição de parada
+		String palavra;
+		do {
+
+			palavra  = MyIO.readString(); //lendo a string digitada pelo usuC!rio
+			char[] caracteres = palavra.toCharArray(); //transformando em vetor
+
+			if (palavra.equalsIgnoreCase("FIM")) { //equalsIgnoreCase serve para identificar se a palavra é igual a FIM
+				break;
+			}
+
+			int tamanho = caracteres.length;  //descobrindo o tamanho do vetor
+
+			for(int i = 0; i < tamanho; i++) {
+
+				caracteres[i] = (char) (caracteres[i] + 3); //trocando a letra pela 3 posterior
+			}
+
+			for (char c : caracteres) {  // for para printar o vetor
+				MyIO.print(c);
+			}
+			MyIO.print();
+			MyIO.println(" ");
+			
+		} while(resp == true);
+
+	}
+}
