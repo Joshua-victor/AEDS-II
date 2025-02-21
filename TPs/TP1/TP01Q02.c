89b357e66e80a21cb9c56fe5e33e5010de3288f0
Joshua Victor <80266019+Joshua-victor@users.noreply.github.com>
2025-02-21 08:56:19 -0300

    Update TP01Q02.c

--- TPs/TP1/TP01Q02.c
+++ TPs/TP1/TP01Q02.c
@@ -4,44 +4,55 @@
 
 int main()
 {
-    
-    char palavra[100];
-    do{
-    bool isfim = true;
-	bool resp = true;  // variavel para falar se é palindromo ou não
+
+	bool isfim = true;
+	do{
+	bool resp = true;  // variavel para falar se C) palindromo ou nC#o
 	char palavra[100];
-	fgets(palavra, sizeof(palavra), stdin); // usa-se fgets para ler palavras com espaço
+	fgets(palavra, sizeof(palavra), stdin); // usa-se fgets para ler palavras com espaC'o
 	palavra[strcspn(palavra, "\n")] = '\0';
 	
-    int tamanho = strlen(palavra); // estipula o tamanho da string
-	
-	if (strcmp(palavra, "FIM") == 0) {
+if (strcmp(palavra, "FIM") == 0) {
             break;
         }
-	
+        
+        
+	char palindromo[100];
+	int tamanho = strlen(palavra) - 1;
+	memcpy(palindromo, palavra, tamanho); // memcpy serve para fazer a copia de vetores
+
 	int j = 0;
-	for (int i = 0; palavra[i] != '\0'; i++) { // for para retirar espaços da string
-        if (palavra[i] != ' ') {
-            palavra[j++] = palavra[i];
-        }
-    }
-    palavra[j] = '\0'; 
-    
-     tamanho = strlen(palavra); //redefinindo o tamanho da palavra sem espaços
-
-	for (int i = 0; i < tamanho / 2; i++) {
-		if (palavra[i] != palavra[tamanho - 1 - i]) {
-			resp = false;
-			i = tamanho;
+	for (int i = 0; palavra[i] != '\0'; i++) { // for para tirar os espaC'os da palavra
+		if (palavra[i] != ' ') {
+			palavra[j++] = palavra[i];
 		}
-	}   
-
-	if(resp) {
-		printf("SIM \n");
-	} else {
-		printf("NÃO \n");
 	}
 
-    }while(1);
-	return 0;
+	for (int i = 0; i < tamanho; i++) { // for para inverter a palavra
+		palindromo[i] = palavra[tamanho - 1 - i];
+	}
+
+
+	for(int i = 0; i < tamanho; i++) { // for para comparar se sC#o iguais
+
+		if(palavra[i] == palindromo[tamanho - 1 - i]) {
+			resp = true;
+		} else {
+			resp =false;
+			i = tamanho;
+		}
+
+
+	}
+
+
+	if(resp == true) {
+		printf("SIM");
+	} else {
+		printf("NAO");
+	}
+	
+	}while(1);
+
 }
+
