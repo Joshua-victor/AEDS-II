8b42c0669c89c5a4552b4f8b375b83f65fc17e89
Joshua Victor <80266019+Joshua-victor@users.noreply.github.com>
2025-02-21 07:58:16 -0300

    Update TP01Q12.c

--- TPs/TP1/TP01Q12.c
+++ TPs/TP1/TP01Q12.c
@@ -5,81 +5,88 @@
 
 int main()
 {
+    do{
 	char palavra[100];
-	fgets(palavra,sizeof(palavra), stdin);
-	palavra[strcspn(palavra, "\n")] = '\0';
+	fgets(palavra,sizeof(palavra), stdin); // ler a palavra 
+	palavra[strcspn(palavra, "\n")] = '\0'; 
 	int tamanho = strlen(palavra);
+	
+	if(strcmp(palavra, "FIM") == 0){
+	    break;
+	}
 
-	bool maiuscula;
-	bool minuscula;
-	bool especial;
-	bool numero;
+	bool maiuscula = false;
+	bool minuscula = false;
+	bool especial = false;
+	bool numero = false;
 	bool check1 = false;
 	bool check2 = false;
 	bool check3 = false;
 	bool check4 = false;
+	int i = 0;
+	
+	if(tamanho < 8){
+	   i = tamanho;
+	}
 
-	for(int i =0 ; i < tamanho; i++) {
+	for(i;  i < tamanho; i++) {
 
-		if(palavra[i] < 'A' || palavra[i] > 'Z' && check1 == false  ) {
+		if(isupper(palavra[i]) && check1 == false) { // isupper verifica se o vetor e Maiusculo
 
 			maiuscula = true;
 			check1 = true;
 
-		} else if(palavra[i] < 'a' || palavra[i] > 'z' && check2 == false ) {
+		} else if(islower(palavra[i]) && check2 == false ) { // islower verifica se o vetor é minusculo
 
 			minuscula = true;
 			check2 = true;
-		} else if(ispunct(palavra[i]) && check3 == false ) {
+			
+		} else if(ispunct(palavra[i]) && check3 == false ) { // ispunct verifica se o vetor é especial
 
 			especial = true;
 			check3 = true;
 
-		} else if(palavra[i] >= '0' && palavra[i] <= '9' && check4 == false ) {
+		} else if(isdigit(palavra[i]) && check4 == false ) { // isdigit verifica se o vetor é numero
 
 			numero = true;
 			check4 = true;
-		}
-
-
+		} 
+		
 	}
 
+/* verificação caso alguma não estava funcionando
 	if(check1) {
 		printf("SIM para maiuscula \n ");
 	} else {
-		printf("NCO para maiuscula \n");
+		printf("NÃO para maiuscula \n");
 	}
 
 	if(check2) {
 		printf("SIM para minuscula \n ");
 	} else {
-		printf("NCO para minuscula \n");
+		printf("NÃO para minuscula \n");
 	}
 
 	if(check3) {
 		printf("SIM para especial \n");
 	} else {
-		printf("NCO para especial \n");
+		printf("NÃO para especial \n");
 	}
 
 
 	if(check4) {
 		printf("SIM para numero  \n");
 	} else {
-		printf("NCO para numero \n");
-	}
+		printf("NÃO para numero \n");
+	}*/
 
 
-	/*
 	if(check1 && check2 && check3 && check4){
-	   printf("SIM");
+	   printf("SIM\n");
 	}else{
-	   printf("NCO");
+	   printf("NÃO\n");
 	}
-
-	*/
-
-
-
+	
+    }while(1);
 	return 0;
 }
