4d54e745017487da32441a592e670d00d8d598f6
Joshua Victor <80266019+Joshua-victor@users.noreply.github.com>
2025-02-21 16:11:23 -0300

    Update TP01Q02.c

--- TPs/TP1/TP01Q02.c
+++ TPs/TP1/TP01Q02.c
@@ -6,55 +6,56 @@ int main()
 {
 
 	bool isfim = true;
-	do{
-	bool resp = true;  // variavel para falar se C) palindromo ou nC#o
-	char palavra[100];
-	fgets(palavra, sizeof(palavra), stdin); // usa-se fgets para ler palavras com espaC'o
-	palavra[strcspn(palavra, "\n")] = '\0';
-	
-if (strcmp(palavra, "FIM") == 0) {
-            break;
-        }
-        
-        
-	char palindromo[100];
-	int tamanho = strlen(palavra) - 1;
-	memcpy(palindromo, palavra, tamanho); // memcpy serve para fazer a copia de vetores
+	do {
+		bool resp = true;  // variavel para falar se é palindromo ou não
+		char palavra[100];
+		fgets(palavra, sizeof(palavra), stdin); // usa-se fgets para ler palavras com espaço
+		palavra[strcspn(palavra, "\n")] = '\0';
 
-	int j = 0;
-	for (int i = 0; palavra[i] != '\0'; i++) { // for para tirar os espaços da palavra
-		if (palavra[i] != ' ') {
-			palavra[j++] = palavra[i];
+		if (strcmp(palavra, "FIM") == 0) {  // caso a palavra for FIM, o programa para
+			break;
 		}
-	}
-
-	for (int i = 0; i < tamanho; i++) { // for para inverter a palavra
-		palindromo[i] = palavra[tamanho - 1 - i];
-	}
 
 
-	for(int i = 0; i < tamanho; i++) { // for para comparar se sC#o iguais
-
-	 if (strcmp(palavra, palindromo) == 0) {
-        
-        resp = true;
-        
-    } else {
-        resp = false;
-        i = tamanho;
-    }
+		char palindromo[100];
+		int tamanho = strlen(palavra);
 
 
-	}
+		int j = 0;
+		for (int i = 0; palavra[i] != '\0'; i++) { // for para tirar os espaços da palavra
+			if (palavra[i] != ' ') {
+				palavra[j++] = palavra[i];
+			}
+		}
+		palavra[j] = '\0'; // acrescentando um \0 no final da string
 
 
-	if(resp == true) {
-		printf("SIM\n");
-	} else {
-		printf("NAO\n");
-	}
-	
-	}while(1);
+		strcpy(palindromo, palavra); // strcpy serve para fazer a copia de vetores
+		tamanho = strlen(palavra);
+
+		for (int i = 0; i < tamanho; i++) { // for para inverter a palavra PALINDROMO
+			palindromo[i] = palavra[tamanho - 1 - i];
+		}
+
+
+		for(int i = 0; i < tamanho; i++) { // for para comparar se são iguais
+
+
+			if (palavra[i] == palindromo[i]) {
+				resp = true;
+			} else {
+				resp = false;
+				i = tamanho;
+			}
+		}
+
+
+		if(resp == true) {
+			printf("SIM\n");
+		} else {
+			printf("NAO\n");
+		}
+
+	} while(1);
 
 }
-
