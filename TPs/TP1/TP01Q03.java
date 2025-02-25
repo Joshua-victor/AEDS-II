2dae1ec156a58fb426b7f8269ee9ad490c9f1a2c
Joshua Victor <80266019+Joshua-victor@users.noreply.github.com>
2025-02-25 07:39:08 -0300

    Update TP01Q03.java

--- TPs/TP1/TP01Q03.java
+++ TPs/TP1/TP01Q03.java
@@ -2,14 +2,14 @@ public class TP01Q03
 {
 	public static void main(String[] args) {
 
-		boolean resp = true; // bool para a condição de parada
+		boolean resp = true; // bool para a condiC'C#o de parada
 		String palavra;
 		do {
 
-			palavra  = MyIO.readString(); //lendo a string digitada pelo usuC!rio
+			palavra  = MyIO.readLine(); //lendo a string digitada pelo usuC!rio
 			char[] caracteres = palavra.toCharArray(); //transformando em vetor
 
-			if (palavra.equalsIgnoreCase("FIM")) { //equalsIgnoreCase serve para identificar se a palavra é igual a FIM
+			if (palavra.equalsIgnoreCase("FIM")) { //equalsIgnoreCase serve para identificar se a palavra C) igual a FIM
 				break;
 			}
 
@@ -18,14 +18,16 @@ public class TP01Q03
 			for(int i = 0; i < tamanho; i++) {
 
 				caracteres[i] = (char) (caracteres[i] + 3); //trocando a letra pela 3 posterior
+
 			}
 
 			for (char c : caracteres) {  // for para printar o vetor
 				MyIO.print(c);
 			}
-			MyIO.print();
-			MyIO.println("");
-			
+
+			MyIO.println(" ");
+
+
 		} while(resp == true);
 
 	}
