5dc871dea3c561e43fb2d88ffae0051322b111fe
Joshua Victor <80266019+Joshua-victor@users.noreply.github.com>
2025-02-26 15:21:26 -0300

    TP01Q08 - 100%

--- TPs/TP1/TP01Q08.java
+++ TPs/TP1/TP01Q08.java
@@ -1,11 +1,13 @@
+import java.util.Scanner;
 public class TP01Q08
 {
 	public static void main(String[] args) {
+	     Scanner scanner = new Scanner(System.in);
 
 		boolean isfim = false;
 		
 		do {
-            String numeros = MyIO.readLine();
+            String numeros = scanner.nextLine();
             
 			if(numeros.equalsIgnoreCase("FIM")) { // condição de parada
 				isfim = true;
@@ -23,10 +25,11 @@ public class TP01Q08
 
 			}
 
-			MyIO.print(soma);
-			MyIO.println("");
+			 System.out.print(soma);
+			 System.out.println(" ");
 
 		} while(!isfim);
+		scanner.close();
 
 	}
 }
