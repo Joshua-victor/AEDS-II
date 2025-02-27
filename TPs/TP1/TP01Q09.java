67cbf4499168e0290495743e917c1da99139e07d
Joshua Victor <80266019+Joshua-victor@users.noreply.github.com>
2025-02-27 17:01:11 -0300

    TP01Q09 -  97%

--- TPs/TP1/TP01Q09.java
+++ TPs/TP1/TP01Q09.java
@@ -1,37 +1,39 @@
+import java.util.Scanner;
 public class TP01Q09
 {
 	public static void main(String[] args) {
+	    Scanner scanner = new Scanner (System.in);
 
 		boolean isfim = false;
 		do {
 
-			String palavra1 = MyIO.readString();
+			String palavra1 = scanner.nextLine().toUpperCase(); // le a string e deixa ela maiuscula
 
-			if(palavra1.equalsIgnoreCase("FIM")) {
+			if(palavra1.equalsIgnoreCase("FIM")) { // função para verficar se palavra1 é igual a FIM
 				isfim = true;
 				break;
 			}
 			
-			String ifem = MyIO.readString();
-			String palavra2 = MyIO.readString();
+			String[] partes = palavra1.split(" - "); // split serve como um separador de string, no caso ela separa cada vez que ve um " - "
+
 			boolean resp = false;
+			char[] vetor1 = partes[0].toCharArray();
+			char[] vetor2 = partes[1].toCharArray();
+			
+	
+			int tamanho = vetor1.length;
 
-			palavra1 = palavra1.toUpperCase(); // função para deixar a palavra completamente maiuscula
-			palavra2 = palavra2.toUpperCase();  // função para deixar a palavra completamente maiuscula
 
-			char[] Palavra1 = palavra1.toCharArray();  // função para transformar a string em vetor
-			char[] Palavra2 = palavra2.toCharArray();  // função para transformar a string em vetor
-			int tamanho = Palavra2.length;
-
-			if(Palavra2.length != Palavra1.length) {
-				resp = false;
-			}
+		 if (vetor1.length != vetor2.length) {
+                System.out.println("\u004E\u00C3\u004F"); 
+                continue; 
+            }
 
 			for(int i = 0; i < tamanho; i++) {
 
 				for(int j = 0; j < tamanho; j++) {
 
-					if(Palavra1[j] == Palavra2[i]) {
+					if(vetor1[j] == vetor2[i] ) {
 						resp = true;
 						j = tamanho;
 
@@ -42,12 +44,13 @@ public class TP01Q09
 			}
 
 			if(resp == true) {
-				MyIO.println("SIM");
-			} else {
-				MyIO.println("NAO");
+				System.out.println("SIM");
+			} else if(resp == false) {
+				System.out.println("\u004E\u00C3\u004F");
 			}
 
 		} while(isfim != true);
+		scanner.close();
 
 	}
 }
