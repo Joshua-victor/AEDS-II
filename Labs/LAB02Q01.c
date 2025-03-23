718c785298ed5347c9487c58b70ed0d10901b218
Joshua Victor <80266019+Joshua-victor@users.noreply.github.com>
2025-03-23 20:36:09 -0300

    Update LAB02Q01.c

--- Labs/LAB02Q01.c
+++ Labs/LAB02Q01.c
@@ -3,28 +3,29 @@
 #include <stdbool.h>
 
 int main() {
+    char linha[201];  
     char palavra1[100], palavra2[100];
 
     while (true) {
-
-        if (fgets(palavra1, sizeof(palavra1), stdin) == NULL || palavra1[0] == '\n') {
-            break;
-        }
-        if (fgets(palavra2, sizeof(palavra2), stdin) == NULL || palavra2[0] == '\n') {
+        if (fgets(linha, sizeof(linha), stdin) == NULL || linha[0] == '\n') {
             break;
         }
 
+      
+        linha[strcspn(linha, "\n")] = '\0';
 
-        palavra1[strcspn(palavra1, "\n")] = '\0';
-        palavra2[strcspn(palavra2, "\n")] = '\0';
+      
+        if (sscanf(linha, "%s %s", palavra1, palavra2) != 2) {
+            break;  
+        }
 
         int tamanho1 = strlen(palavra1);
         int tamanho2 = strlen(palavra2);
 
         char combinatoria[tamanho1 + tamanho2 + 1];
-        int i = 0, k = 0;
+        int k = 0;
 
-        for (i = 0; i < tamanho1 + tamanho2; i++) {
+        for (int i = 0; i < tamanho1 || i < tamanho2; i++) {
             if (i < tamanho1) {
                 combinatoria[k++] = palavra1[i];
             }
