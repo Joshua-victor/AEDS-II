4e669df5479e67fcb56a1649e4cbbd62151ec3e4
Joshua Victor <80266019+Joshua-victor@users.noreply.github.com>
2025-04-01 08:38:38 -0300

    começando Tp por aqui

---
+++ TPs/TP2/Show.java
@@ -0,0 +1,117 @@
+import java.util.Date;
+
+public class Show {
+
+    private String SHOW_ID;
+    private String TYPE;
+    private String TITLE;
+    private String DIRECTOR;
+    private String[] CAST;
+    private String COUNTRY;
+    private Date DATE_ADDED;
+    private int RELEASE_YEAR;
+    private String RATING;
+    private String DURATION;
+    private String[] LISTED_IN;
+
+
+    public Show (String SHOW_ID, String TYPE, String TITLE, String DIRECTOR, String[] CAST, String COUNTRY, Date DATE_ADDED, int RELEASE_YEAR, String RATING, String DURATION,  String[] LISTED_IN ){
+
+        this.SHOW_ID = SHOW_ID;
+        this.TYPE = TYPE;
+        this.TITLE = TITLE;
+        this.DIRECTOR = DIRECTOR;
+        this.CAST = CAST;
+        this.COUNTRY = COUNTRY;
+        this.DATE_ADDED = DATE_ADDED;
+        this.RELEASE_YEAR = RELEASE_YEAR;
+        this.RATING = RATING;
+        this.DURATION = DURATION;
+        this.LISTED_IN = LISTED_IN;
+    }
+
+
+       //getters
+        public String getSHOW_ID() {
+            return SHOW_ID;
+        }
+        public String getTYPE() {
+            return TYPE;
+        }
+        public String getTITLED() {
+            return TITLE;
+        }
+        public String getDIRECTOR() {
+            return DIRECTOR;
+        }
+        public String getCAST() {
+            return CAST;
+        }
+        public String getCOUNTRY() {
+            return COUNTRY;
+        }
+        public Date getDATE_ADDED() {
+            return DATE_ADDED;
+        }
+        public String getRELEASE_YEAR() {
+            return RELEASE_YEAR;
+        }
+        public String getRELEASE_YEAR() {
+            return RELEASE_YEAR;
+        }
+        public String getRELEASE_YEAR() {
+            return RELEASE_YEAR;
+        }
+        public String getRELEASE_YEAR() {
+            return RELEASE_YEAR;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+   
+
+
+
+
+
+    
+}   
+
