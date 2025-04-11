8b05e3b9d072aa78da9e5c76f11ff384f8e8fdae
Joshua Victor <80266019+Joshua-victor@users.noreply.github.com>
2025-04-11 16:24:43 -0300

    Update Show.java

--- TPs/TP2/Show.java
+++ TPs/TP2/Show.java
@@ -5,6 +5,8 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
+import java.util.Arrays;
+
 
 
  class Show {
@@ -161,23 +163,24 @@ import java.util.Scanner;
     
     public void mostrar() {
 
-        System.out.println("=>"
-            + this.getShowId() + " ## "
-            + this.getType() + " ## "
-            + this.getTitle() + " ## "
-            + (this.getDirector() == "" ? "NaN" : this.getDirector()) + " ## "
-            +"[" + (this.getCast() == null ? "NaN" : this.getCast()) + "] " + " ## "
-            + (this.getCountry() == "" ? "NaN" : this.getCountry()) + " ## "
-            + (this.getDateAdded() == null ? "NaN" : this.getDateAdded()) + " ## "
-            + (this.getReleaseYear() == 0 ? "NaN" : this.getReleaseYear()) + " ## "
-            + (this.getRating() == null ? "NaN" : this.getRating()) + " ## "
-            + (this.getDuration() == "" ? "NaN" : this.getDuration()) + " ## "
-            +"[" + (this.getListedIn() == null ? "NaN" : this.getListedIn()) + "] " + " ## ");
+         System.out.println("=> "
+        + this.getShowId() + " ## "
+        + this.getTitle() + " ## "
+        + this.getType() + " ## "
+        + this.getDirector() + " ## "
+        + Arrays.toString(this.getCast()) + " ## "
+        + this.getCountry() + " ## "
+        + this.getDateAdded() + " ## "
+        + this.getReleaseYear() + " ## "
+        + this.getRating() + " ## "
+        + this.getDuration() + " ## "
+        + Arrays.toString(this.getListedIn()) + " ## ");
     }
     
     
     public void ler(String linha){
         SimpleDateFormat sdf = new SimpleDateFormat("MMMM d, yyyy");
+       // System.out.print("ENTREI");
         
        try{
            
@@ -185,7 +188,7 @@ import java.util.Scanner;
            
            this.showId = partes[0]; // alocando em showId a posição 0 de partes
            this.type = partes[1]; // alocando em type a posição 1 de partes
-           this.title = partes[2];// alocando em title a posição 2 de partes
+           this.title = partes[2].replaceAll("^\"|\"$", "").replaceAll("\"\"", ""); // alocando em title a posição 2 de partes
            this.director = partes[3].isEmpty() ? "NaN" : partes[3]; // if partes[3] vazia this.director = "NaN" else this.director = partes[3]
            this.cast = partes[4].isEmpty() ? new String[] { "NaN" } : partes[4].replace("\"", "").split(",");;
            this.country = partes[5].isEmpty() ? "NaN" : partes[5]; // alocando em country a posição 5 de partes
@@ -197,43 +200,52 @@ import java.util.Scanner;
            this.releaseYear = partes[7].isEmpty() ? 0 : Integer.parseInt(partes[7]); //Integer.parseInt usado para trasnformar uma string em Int
            this.rating = partes[8].isEmpty() ? "NaN" : partes[8];// alocando em rating a posição 8 de partes
            this.duration = partes[9].isEmpty() ? "NaN" : partes[9];// alocando em duration a posição 9 de partes
-           this.listedIn = partes[10].isEmpty() ? new String[] { "NaN" } : partes[10].replace("\"", "").split(",");;
+           this.listedIn = partes[10].isEmpty() ? new String[] { "NaN" } : partes[10].replace("\"","").split(",");
            
        } catch(Exception e){
            
            System.out.println("Erro ao ler a linha" + linha);
-           
        }
-        
-
     }
-    
-    
-
-    
 }
+
+
 
 public class Main {
-    public static void main(String[] args) {
-    Scanner Scanner =  new Scanner (System.in);
-        
-    ArrayList<String> lista = new ArrayList<>();    
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> lista = new ArrayList<>();
 
-    
-        
-     while(true){ 
-     String linha = Scanner.nextLine();
-     if(linha.equalsIgnoreCase("FIM")){
-         break;
-     }
-     
-    
-        
-        
-       Scanner.close();
-  
-    }
+        // Lê todos os IDs digitados
+        while (true) {
+            String linha = scanner.nextLine();
+            if (linha.equalsIgnoreCase("FIM")) break;
+            lista.add(linha);
+        }
+
+       
+       BufferedReader br = new BufferedReader(new FileReader("disneyplus.csv")); // leitura do arquivo 
+       String CSV;
+       br.readLine(); //pula a primeira linha do cabeçalho do CSV
+       
+       while((CSV = br.readLine()) != null){
+           
+           Show show = new Show();
+           show.ler(CSV);
+           
+           if (lista.contains(show.getShowId())) {
+                show.mostrar();
+           }
+           
+   
+       }
+       
+       br.close();
+       
+       
+       
+       
 }
-
+}
 
 
